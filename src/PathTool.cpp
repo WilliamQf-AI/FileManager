b61a80d9a8022592ab3002d4dc5ee3e3aabfb7d6
unknown <412588801@qq.com>
2024-02-23 15:44:08 +0800

    hover tool bar btn

--- src/PathTool.cpp
+++ src/PathTool.cpp
@@ -21,11 +21,19 @@ void PathTool::resize(const int& w, const int& h)
 void PathTool::paint(SkCanvas* canvas)
 {
 	if (!needPaint(canvas)) return;
+	SkPaint paint;
+	if (hoverIndex >= 0) {
+		paint.setColor(0x18000000);  //0xFFA8C7FA
+		auto r = SkRect::MakeXYWH(10.f + hoverIndex * 50.f, rect.fTop + 11.f, 36.f, 36.f);
+		canvas->drawRoundRect(r,6.f,6.f, paint);
+		paint.setColor(0xFF222222);
+	}
+	else {
+		paint.setColor(0xFF666666);
+	}
 	auto font = App::GetFontIcon();
 	font->setSize(26.f);
 	auto iconCode = (const char*)u8"\ue80b";
-	SkPaint paint;
-	paint.setColor(0xFF666666);
 	float start{ 16.f };
 	float top{ rect.fTop + 39.f };
 	canvas->drawString(iconCode, start, top, *font, paint);
@@ -35,4 +43,20 @@ void PathTool::paint(SkCanvas* canvas)
 	canvas->drawString(iconCode, start + 100, top, *font, paint);
 	iconCode = (const char*)u8"\ue741";
 	canvas->drawString(iconCode, start + 150, top, *font, paint);
+}
+
+void PathTool::mouseMove(const int& x, const int& y)
+{
+	int index = -1;
+	if (rect.contains(x, y) && x > 8.f) {
+		index = (x-8.f) / 50;
+	}
+	if (index != hoverIndex) {
+		hoverIndex = index;
+		repaint();
+	}
+}
+
+void PathTool::mouseDown(const int& x, const int& y)
+{
 }
